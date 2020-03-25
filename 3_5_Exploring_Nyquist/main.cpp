594fc3db33be99f7dfeeb545ac552ef851bf2725
nthuee106061135 <frank880415@gmail.com>
2020-03-25 20:41:10 +0800

    mbed03_5

---
+++ 3_5_Exploring_Nyquist/main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+
+
+AnalogOut Aout(DAC0_OUT);
+
+AnalogIn Ain(A0);
+
+float ADCdata;
+
+
+int main(){
+
+  while(1){
+
+    ADCdata = Ain;
+
+    Aout = ADCdata;
+
+    wait(0.002);
+
+  }
+
+}
